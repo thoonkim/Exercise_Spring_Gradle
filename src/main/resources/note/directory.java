5d73eed50da53a318d4c32a233242569e984e522
thoonkim <thoon.kim12@gmail.com>
2022-02-25 12:44:05 +0900

    20220224.Spring Project2 부족부분 공부

--- src/main/resources/note/directory.java
+++ src/main/resources/note/directory.java
@@ -3,8 +3,8 @@ gradle //그레이들 관련한 폴더
 src/main //자바 소스파일
 src/test //test코드와 관련된 소스
 resources //설정, html 등 java 파일을 제외한 모든 파일
-build.gradle //중요)
+build.gradle //중요) 의존성이나 플러그인 설정 등을 위한 스크립트
 .gitignore //깃에는 빌드 결과물은 올라가면안되고 소스코드만 올라가야하는데 그것을 제어해주는 것
-gradlew
-gradlew.bat
-settings.gradle //
+gradlew //UNIX script
+gradlew.bat //Windows명령이 jar 파일 및 속성 파일과 함께 생성
+settings.gradle // 프로젝트 수준 저장소 설정을 정의하고, 앱을 빌드할 때 포함해야 하는 모듈을 Gradle에 알려줌
